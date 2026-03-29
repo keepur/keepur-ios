2bc0e2e96e25d39f08b04a7a54c05d2fa0f8dabc
Mokie Huang <mokie.huang@gmail.com>
2026-03-29 15:39:01 -0700

    Initial Commit

---
+++ Keepur/KeepurApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepurApp.swift
+//  Keepur
+//
+//  Created by Mokie Huang on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct KeepurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
